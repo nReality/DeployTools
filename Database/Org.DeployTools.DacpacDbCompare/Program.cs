92e0be35ea647c48d914b418fb5a9024e1e0fb1d
Hendri Pretorius <hendri@nreality.com>
2015-11-01 20:00:57 +0200

    add DacpacDbCompare project

---
+++ Database/Org.DeployTools.DacpacDbCompare/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Org.DeployTools.Shared;
+
+namespace Org.DeployTools.DacpacDbCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var connectionStringBuilder = new SqlConnectionStringBuilder
+                {
+                    DataSource = args[0],
+                    InitialCatalog = args[1],
+                    IntegratedSecurity = true
+                };
+                CreateScript(DefaultSettings.SqlPackagePath, connectionStringBuilder, args[2], args[3], args[4]);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+                Console.Error.WriteLine(ex.StackTrace);
+                Environment.Exit(1);
+            }
+        }
+
+        private static void CreateScript(string sqlpackage, DbConnectionStringBuilder connection, string dacpacFile, string profileFile, string outputFile)
+        {
+            const string argumentsMask = "/Action:Script /OverwriteFiles:True /Quiet:False /TargetConnectionString:\"{0}\" /SourceFile:{1} /Profile:{2} /OutputPath:{3}";
+            var arguments = string.Format(argumentsMask, connection.ConnectionString, dacpacFile, profileFile, outputFile);
+            ExternalProcessExecutor.Exec(sqlpackage, arguments);
+        }
+    }
+}
---
+++ Org.DeployTools.Shared/DefaultSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Org.DeployTools.Shared
+{
+    public class DefaultSettings
+    {
+        public const string SqlPackagePath = @"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe";
+    }
+}
