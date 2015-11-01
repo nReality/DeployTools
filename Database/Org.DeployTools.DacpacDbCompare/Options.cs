a38d2743ad376a94eb4b054840ae601469335f0d
Hendri Pretorius <hendri@nreality.com>
2015-11-01 21:48:12 +0200

    extract ConnectionStringOptions
    base class for parsing command line options with connection strings

--- Database/Org.DeployTools.DacpacDbCompare/Options.cs
+++ Database/Org.DeployTools.DacpacDbCompare/Options.cs
@@ -1,15 +1,10 @@
-﻿using System.Data.SqlClient;
-using CommandLine;
+﻿using CommandLine;
+using Org.DeployTools.Shared.CommandLineOptions;
 
 namespace Org.DeployTools.DacpacDbCompare
 {
-    class Options
+    class Options : ConnectionStringOptions
     {
-        [Option("server", HelpText = "Server to compare against", Required = true)]
-        public string Server { get; set; }
-        [Option("database", HelpText = "Database to compare against", Required = true)]
-        public string Database { get; set; }
-
         [Option("dacpac", HelpText = "Dacpac file to use as input", Required = true)]
         public string DacpacFile { get; set; }
         [Option("profile", HelpText = "Profile file to use with comparing", Required = true)]
@@ -17,15 +12,5 @@ namespace Org.DeployTools.DacpacDbCompare
 
         [Option("output", HelpText = "Output script file", Required = true)]
         public string OutputFile { get; set; }
-
-        public SqlConnectionStringBuilder ConnectionStringBuilder()
-        {
-            return new SqlConnectionStringBuilder
-            {
-                DataSource = Server,
-                InitialCatalog = Database,
-                IntegratedSecurity = true
-            };
-        }
     }
 }
---
+++ Org.DeployTools.Shared/CommandLineOptions/ConnectionStringOptions.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+using CommandLine;
+
+namespace Org.DeployTools.Shared.CommandLineOptions
+{
+    public class ConnectionStringOptions
+    {
+        [Option("server", HelpText = "Server to compare against", Required = true)]
+        public string Server { get; set; }
+
+        [Option("database", HelpText = "Database to compare against", Required = true)]
+        public string Database { get; set; }
+
+        public SqlConnectionStringBuilder ConnectionStringBuilder()
+        {
+            return new SqlConnectionStringBuilder
+            {
+                DataSource = Server,
+                InitialCatalog = Database,
+                IntegratedSecurity = true
+            };
+        }
+    }
+}
