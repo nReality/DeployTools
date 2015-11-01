92b081d83dbde31f504f7f152784640c149f4317
Hendri Pretorius <hendri@nreality.com>
2015-11-01 19:52:08 +0200

    setup solution, add shared project
    add ExternalProcessExecutor

---
+++ Org.DeployTools.Shared/ExternalProcessExecutor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Org.DeployTools.Shared
+{
+    public class ExternalProcessExecutor
+    {
+        public static void Exec(string path, string arguments)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo(path, arguments)
+                {
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false
+                }
+            };
+            process.Start();
+            ReadProcessOutput(process);
+            process.WaitForExit();
+            if (process.ExitCode > 0)
+                throw new Exception(string.Format("External process exited with code {0}", process.ExitCode));
+        }
+
+        private static void ReadProcessOutput(Process process)
+        {
+            while (!process.StandardOutput.EndOfStream)
+            {
+                process.StandardOutput.BaseStream.Flush();
+                Console.Write(process.StandardOutput.ReadToEnd());
+            }
+        }
+    }
+}
