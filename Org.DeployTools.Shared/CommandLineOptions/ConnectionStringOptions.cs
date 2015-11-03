54fb0b36eb0b179801e29772f351bad45c0cc16c
Hendri Pretorius <hendri@nreality.com>
2015-11-03 11:43:06 +0200

    allow sql autentication for connection string options

--- Org.DeployTools.Shared/CommandLineOptions/ConnectionStringOptions.cs
+++ Org.DeployTools.Shared/CommandLineOptions/ConnectionStringOptions.cs
@@ -14,9 +14,28 @@ namespace Org.DeployTools.Shared.CommandLineOptions
         [Option("integrated", HelpText = "Use integrated security for authentication")]
         public bool UseIntegratedSecutory { get; set; }
 
+        [Option("username", HelpText = "Username to use with authentication")]
+        public string Username { get; set; }
+
+        [Option("password", HelpText = "Password to use with authentication")]
+        public string Password { get; set; }
+
         public SqlConnectionStringBuilder ConnectionStringBuilder()
         {
-            return IntegratedSecutirySqlConnectionStringBuilder();
+            return UseIntegratedSecutory
+                ? IntegratedSecutirySqlConnectionStringBuilder()
+                : SqlSecutiryConnectionStringBuilder();
+        }
+
+        private SqlConnectionStringBuilder SqlSecutiryConnectionStringBuilder()
+        {
+            return new SqlConnectionStringBuilder
+            {
+                DataSource = Server,
+                InitialCatalog = Database,
+                UserID = Username,
+                Password = Password
+            };
         }
 
         private SqlConnectionStringBuilder IntegratedSecutirySqlConnectionStringBuilder()
--- Org.DeployTools.Tests/CommandLineOptions/ConnectionStringTests.cs
+++ Org.DeployTools.Tests/CommandLineOptions/ConnectionStringTests.cs
@@ -13,11 +13,26 @@ namespace Org.DeployTools.Tests.CommandLineOptions
             var options = new ConnectionStringOptions();
             var passed = CommandLine.Parser.Default.ParseArguments(args, options);
 
-            Assert.IsTrue(passed);
+            Assert.IsTrue(passed, "failed to parse args");
             var builder = options.ConnectionStringBuilder();
             Assert.AreEqual("server", builder.DataSource);
             Assert.AreEqual("database", builder.InitialCatalog);
             Assert.AreEqual(true, builder.IntegratedSecurity);
+        }
+
+        [Test]
+        public void CanHaveSqlAuthentication()
+        {
+            var args = new[] {"--server", "server", "--database", "database", "--username", "user", "--password", "pass"};
+
+            var options = new ConnectionStringOptions();
+            var passed = CommandLine.Parser.Default.ParseArguments(args, options);
+
+            Assert.IsTrue(passed, "failed to parse args");
+            var builder = options.ConnectionStringBuilder();
+            Assert.AreEqual(false, builder.IntegratedSecurity);
+            Assert.AreEqual("user", builder.UserID);
+            Assert.AreEqual("pass", builder.Password);
         }
     }
 }
