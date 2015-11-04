f74c43dd678c0e0169cc02604eb62c217a8b988e
Hendri Pretorius <hendri@nreality.com>
2015-11-04 11:11:10 +0200

    implement BacPackage importing and exporting

--- Database/Org.DeployTools.BacPackage/Program.cs
+++ Database/Org.DeployTools.BacPackage/Program.cs
@@ -1,4 +1,6 @@
-﻿using Org.DeployTools.Shared;
+﻿using System;
+using Org.DeployTools.Shared;
+using Microsoft.SqlServer.Dac;
 
 namespace Org.DeployTools.BacPackage
 {
@@ -11,6 +13,34 @@ namespace Org.DeployTools.BacPackage
 
         private static void Run(Options options)
         {
+            if (options.Action == Options.BacpacAction.Export)
+            {
+                Export(options);
+            }
+            else if (options.Action == Options.BacpacAction.Import)
+            {
+                Import(options);
+            }
+        }
+
+        private static void Import(Options options)
+        {
+            var package = Microsoft.SqlServer.Dac.BacPackage.Load(options.BacpacFile);
+            var service = new DacServices(options.ConnectionStringBuilder().ConnectionString);
+            service.ProgressChanged += ServiceOnProgressChanged;
+            service.ImportBacpac(package, options.ConnectionStringBuilder().InitialCatalog);
+        }
+
+        private static void Export(Options options)
+        {
+            var service = new DacServices(options.ConnectionStringBuilder().ConnectionString);
+            service.ProgressChanged += ServiceOnProgressChanged;
+            service.ExportBacpac(options.BacpacFile, options.ConnectionStringBuilder().InitialCatalog);
+        }
+
+        private static void ServiceOnProgressChanged(object sender, DacProgressEventArgs dacProgressEventArgs)
+        {
+            Console.WriteLine("{0} {1}", dacProgressEventArgs.Message, dacProgressEventArgs.Status);
         }
     }
 }
