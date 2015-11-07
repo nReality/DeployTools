b8f068fe15a59012bd516891b161ff9bab89465a
Hendri Pretorius <hendri@nreality.com>
2015-11-07 12:16:14 +0200

    SqlcmdArguments builder
    build integrated security command line arguments

---
+++ Org.DeployTools.Shared/ExternalProcessArgumentBuilder/SqlcmdArguments.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.SqlClient;
+
+namespace Org.DeployTools.Shared.ExternalProcessArgumentBuilder
+{
+    public class SqlcmdArguments
+    {
+        public static string Build(SqlConnectionStringBuilder connection)
+        {
+            const string argumentsMaskIntegrated = "-S \"{0}\" -d \"{1}\" ";
+            return string.Format(argumentsMaskIntegrated, connection.DataSource, connection.InitialCatalog);
+        }
+    }
+}
---
+++ Org.DeployTools.Tests/ExternalProcessArguments/SqlcmdTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
+using Org.DeployTools.Shared.ExternalProcessArgumentBuilder;
+
+namespace Org.DeployTools.Tests.ExternalProcessArguments
+{
+    internal class SqlcmdTests
+    {
+        private SqlConnectionStringBuilder _sqlConnectionStringBuilder;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sqlConnectionStringBuilder = new SqlConnectionStringBuilder
+            {
+                DataSource = "s",
+                InitialCatalog = "d",
+            };
+        }
+
+        [Test]
+        public void IntegratedSecurity()
+        {
+            _sqlConnectionStringBuilder.IntegratedSecurity = true;
+
+            var arguments = SqlcmdArguments.Build(_sqlConnectionStringBuilder);
+
+            Assert.IsTrue(arguments.Contains("-S \"s\" -d \"d\""), "server and database details missing in " + arguments);
+        }
+    }
+}
