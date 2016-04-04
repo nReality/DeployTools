b4550cb0d7cb6f43fcb6a661cc46dba1d426c25c
Hendri Pretorius <hendri@nreality.com>
2016-04-04 18:18:04 +0200

    start sc.exe with the given parameters from the command line

--- Org.DeployTools.ServiceControl/Program.cs
+++ Org.DeployTools.ServiceControl/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Org.DeployTools.ServiceControl
+﻿using Org.DeployTools.Shared;
+
+namespace Org.DeployTools.ServiceControl
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SafeMain.ParseArgumentsAndRunInTryCatch<Options>(args, Run);
+        }
+
+        private static void Run(Options options)
+        {
+            var arguments = string.Format("{0} {1} {2}", options.Servername, options.Action.ToString().ToLower(), options.ServiceName);
+            ExternalProcessExecutor.Exec(DefaultSettings.ServiceControl, arguments);
         }
     }
 }
--- Org.DeployTools.Shared/DefaultSettings.cs
+++ Org.DeployTools.Shared/DefaultSettings.cs
@@ -6,5 +6,6 @@
         public const string SqlcmdPath = @"C:\Program Files\Microsoft SQL Server\110\Tools\Binn\SQLCMD.EXE";
         public const string SqlPackagePath = @"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\sqlpackage.exe";
         public const string Msdeploy = @"C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe";
+        public const string ServiceControl = "sc.exe";
     }
 }
