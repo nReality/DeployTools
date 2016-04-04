2bb381ebd15626835d8c92a926dc4bc6d85f64c0
Hendri Pretorius <hendri@nreality.com>
2016-04-04 18:35:42 +0200

    get the exit reason based on SC exit code and action

--- Org.DeployTools.ServiceControl/Program.cs
+++ Org.DeployTools.ServiceControl/Program.cs
@@ -1,9 +1,13 @@
 ﻿using Org.DeployTools.Shared;
+using System;
 
 namespace Org.DeployTools.ServiceControl
 {
     class Program
     {
+        const int AlreadyRunningExitCode = 1056;
+        const int NotStartedExitCode = 1062;
+
         static void Main(string[] args)
         {
             SafeMain.ParseArgumentsAndRunInTryCatch<Options>(args, Run);
@@ -12,7 +16,31 @@ namespace Org.DeployTools.ServiceControl
         private static void Run(Options options)
         {
             var arguments = string.Format("{0} {1} {2}", options.Servername, options.Action.ToString().ToLower(), options.ServiceName);
-            ExternalProcessExecutor.Exec(DefaultSettings.ServiceControl, arguments);
+            int exitCode = ExternalProcessExecutor.ExecAndGetExitCode(DefaultSettings.ServiceControl, arguments);
+            ValidateExitCode(options, exitCode);
+        }
+
+        private static void ValidateExitCode(Options options, int exitCode)
+        {
+            string reason = GetExitReason(options, exitCode);
+
+            if (string.IsNullOrEmpty(reason))
+            {
+                ExternalProcessExecutor.ValidateExitWithZero(exitCode);
+            }
+            else
+            {
+                Console.Error.WriteLine("SC reported failure, but exiting with success ({0})", reason);
+            }
+        }
+
+        private static string GetExitReason(Options options, int exitCode)
+        {
+            if (exitCode == AlreadyRunningExitCode && options.Action == Options.ScAction.Start)
+                return "Service already running, so start had no effect";
+            if (exitCode == NotStartedExitCode && options.Action == Options.ScAction.Stop)
+                return "Service not started, so stop had no effect";
+            return null;
         }
     }
 }
